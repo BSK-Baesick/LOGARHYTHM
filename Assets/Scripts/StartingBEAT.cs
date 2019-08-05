b2282aa3cc592998423c1a70686d87d048f5de96
Matthew Johannes C. Balubar <matthew.balubar@gmail.com>
2019-08-05 22:03:02 +0800

    v1.0.1a

--- Assets/Scripts/StartingBEAT.cs
+++ Assets/Scripts/StartingBEAT.cs
@@ -7,7 +7,8 @@ public class StartingBEAT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  Set the coordinates of the Starting BEAT at the origin of the game world.
+        transform.position = new Vector2(0, 0);
     }
 
     // Update is called once per frame
