99076720952cd706ca7360b292169579d480bd50
Matthew Johannes C. Balubar <matthew.balubar@gmail.com>
2019-08-05 23:04:01 +0800

    v1.2.1a

--- Assets/Scripts/StartingBEAT.cs
+++ Assets/Scripts/StartingBEAT.cs
@@ -1,15 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HedgehogTeam.EasyTouch;
+
+/// <summary>
+/// This script handles the Starting BEAT game logic.
+/// </summary>
 
 public class StartingBEAT : MonoBehaviour
 {
+
+    // Configuration Parameters
+    [Header("Starting BEAT Components")]
+    [SerializeField] AudioClip levelSoundtrack = null;
+
+    // Subscribe
+    private void OnEnable()
+    {
+        EasyTouch.On_SimpleTap += EasyTouch_On_SimpleTap;
+    }
+
+    // Unsubscribe
+    private void OnDisable()
+    {
+        EasyTouch.On_SimpleTap -= EasyTouch_On_SimpleTap;
+    }
+
+    private void OnDestroy()
+    {
+        EasyTouch.On_SimpleTap -= EasyTouch_On_SimpleTap;
+    }
+
+    private void EasyTouch_On_SimpleTap(Gesture gesture)
+    {
+        // Play the level soundtrack
+        if (levelSoundtrack != null)
+        {
+            var cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(levelSoundtrack, cameraPos);
+        }
+
+        // Destroy this gameObject
+        Destroy(this.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //  Set the coordinates of the Starting BEAT at the origin of the game world.
         transform.position = new Vector2(0, 0);
     }
+
+
 
     // Update is called once per frame
     void Update()
